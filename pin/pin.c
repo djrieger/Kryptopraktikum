7f25eb90d7a2aef4ab3ddbe1ff98a122a5f8fd47
David Rieger <djrieger@users.noreply.github.com>
2014-11-12 12:40:32 +0100

    irgendwie kaputt

--- pin/pin.c
+++ pin/pin.c
@@ -11,6 +11,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #include "pin.h"
 
@@ -22,7 +23,7 @@ int pin[9000], prob[9000], trypin[9000];
 
 void attack(void)
 {
-    int i, pin1, pin2, pin3, pin4;
+    int i, j, pin1, pin2, pin3, pin4;
     int pins[65536][4];
 
     // prob = 0
@@ -40,21 +41,32 @@ void attack(void)
     			for (pin4 = 0; pin4 < 16; pin4++)
     			{
     				pins[i][0] = map(pin1);
-    				if (pins[i][0] == 0)
-    					pins[i][0] = 1;
+    				//if (pins[i][0] == 0)
+    				//	pins[i][0] = 1;
     				pins[i][1] = map(pin2);
     				pins[i][2] = map(pin3);
     				pins[i][3] = map(pin4);
 
+    				printf("%d%d%d%d\n", pins[i][0], pins[i][1], pins[i][2], pins[i][3]);
+
+
     				//printf("%d%d%d%d == ", pins[i][0], pins[i][1], pins[i][2], pins[i][3]);
-    				int index = pins[i][3] + 10 * pins[i][2] + 100 * pins[i][1] + 1000 * pins[i][0] - 1000;
+    				int pinCandidate = pins[i][3] + 10 * pins[i][2] + 100 * pins[i][1] + 1000 * pins[i][0];
+
+    				char diffString[5];
+    				//itoa(diff1, diffString, 10);
+    				sprintf(diffString,"%d",diff1);
+    				int index = 0;
+    				for (j = 3; j >= 0; j--)
+    					index += (((diffString[j] - '0') - pins[i][j]) % 10) * pow(10, 3 - j);
+    				printf("%d/%s - %d = %d\n", diff1, diffString, pinCandidate, index);
     				//printf("%d", index);
-    				prob[index]++;
+    				prob[index - 1000]++;
     				//printf(" (prob = %d)\n", prob[index]);
 
     				i++;
-    				//if (i >= 50)
-    				//	return 0;
+    				if (i >= 50)
+    					return 0;
     			}
 
 //    printf("---------\n");
@@ -64,7 +76,7 @@ void attack(void)
 	// sort
 	int highestProb = -1;
 	int highestProbIndex = -1;
-	int j;
+	
 	for (i = 0; i < 100; i++)
     {
     	for (j = 0; j < 9000; j++)
@@ -82,7 +94,7 @@ void attack(void)
     	highestProb = -1;
     	prob[highestProbIndex] = -1;
     }
-
+    //printf("%d\n", try_pins(trypin, 100));
 }
 
 int map(int c)
@@ -93,6 +105,8 @@ int map(int c)
 int main(void)
 {
 	open_connection(0, &diff1, &diff2);
+	printf("diff1 %d, diff2 %d\n", diff1, diff2);
+	
 	attack();
 	close_connection();
 	exit(0);
