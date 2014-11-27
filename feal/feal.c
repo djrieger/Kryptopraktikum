b7eb5e27fc3662f565e3e2dfcec2f9b29d95b363
David Rieger <djrieger@users.noreply.github.com>
2014-11-27 12:15:20 +0100

    Bitchecks implementiert

--- feal/feal.c
+++ feal/feal.c
@@ -51,28 +51,90 @@ void printBits(size_t const size, void const * const ptr)
             byte = b[i] & (1<<j);
             byte >>= j;
             printf("%u", byte);
+            if (j % 2 == 0)
+              printf(" ");
         }
     }
-    puts("");
+    //puts("");
 }
 
 
-void test(ubyte u, ubyte v, int *keyoverflow) {
-  ubyte w = Feal_GS(u, v, keyoverflow);
+ubyte test(ubyte u, ubyte v, int *keyoverflow) {
+  ubyte w = rotr(Feal_GS(u, v, keyoverflow));
   printf("u %d v %d w %d (", u, v, w);
   printBits(sizeof(w), &w);
-  printf(")\n");
+  printf(") overflow %d\n", *keyoverflow);
+  return w;
+}
+
+int lastButOneBitSet(ubyte u)
+{
+  return ((1 << 1) & u) / 2;
+}
+
+int findBitWithValue(ubyte number[4], int bitValue) {
+  int i;
+  for (i = 0; i < 4; i++) {
+    if (lastButOneBitSet(number[i]) == bitValue) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+int findSpecialBit(ubyte number[4], int *bitValue) {
+  int index, i;
+  int bitCount = 0;
+  // sum last but one column
+  for (i = 0; i < 4; i++) {
+    bitCount += lastButOneBitSet(number[i]);
+  }
+  if (bitCount == 3) {
+    index = findBitWithValue(number, 0);
+  } else if (bitCount == 1) {
+    index = findBitWithValue(number, 1);
+  } else {
+    printf("ERRROR\n");
+  }
+  *bitValue = lastButOneBitSet(number[index]);
+  return index;
+}
+
+void test2() {
+  int keyoverflow;
+  ubyte w[4];
+  w[0] = test(0, 0, &keyoverflow);
+  w[1] = test(1, 0, &keyoverflow);
+  w[2] = test(0, 1, &keyoverflow);
+  w[3] = test(1, 1, &keyoverflow);
+  // vorletztes Bit gesetzt?
+  int lastButOneBitValue;
+  int specialBit = findSpecialBit(w, &lastButOneBitValue);
+  printf("special at %d, value %d\n", specialBit, lastButOneBitValue);
+
+  /*
+    int bitSet = lastButOneBitSet(w[i]);
+    printf("bit %d", bitSet);
+    
+    for (j = 0; j < 4; j++) {
+      if (j != i) {
+        sameBitSet += lastButOneBitSet(w[i]) == lastButOneBitSet(w[j]);
+        if (lastButOneBitSet(w[i]) != lastButOneBitSet(w[j])) {
+          index = j;
+        }
+      }
+    }
+    printf("special bit at %d, same bits %d\n", index, sameBitSet);
+  }
+  */
 }
 
 int main(int argc, char **argv)
 {
   ubyte k1,k2,k3;
   Feal_NewKey();
-  int keyoverflow;
-  test(0, 0, &keyoverflow);
-  test(1, 0, &keyoverflow);
-  test(0, 1, &keyoverflow);
-  test(1, 1, &keyoverflow);
+  test2();
+
   /*>>>>                                                      <<<<*/
   /*>>>>  Aufgabe: Bestimmen der geheimen Schlüssel k1,k2,k3  <<<<*/
   /*>>>>                                                      <<<<*/
